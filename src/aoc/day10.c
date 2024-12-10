aa8624a3b4347f3438ad62e3ab70e2e35ac7d121
Simon Renhult <simon.renhult@hotmail.se>
2024-12-10 20:06:52 +0100

    Dev: Refactor day 10

--- src/aoc/day10.c
+++ src/aoc/day10.c
@@ -7,35 +7,32 @@ global Str8 day10_example = str8_literal(
     "32019012\n"
     "01329801\n"
     "10456732\n"
-
 );
 
-internal V2U64 day10_find_9(U8 *map, R2S64 bounds, V2S64 point, U8 wanted) {
+internal V2U64 day10_find_9(B8 *visited, U8 *map, R2S64 bounds, V2S64 point, U8 wanted) {
+    V2U64 result = v2u64(0, 0);
+
     S64 index = point.x + point.y * r2s64_size(bounds).width;
+    if (map[index] == wanted) {
+        if (map[index] == '9') {
+            result = v2u64(!visited[index], 1);
+            visited[index] = wanted == '9';
+        } else {
+            V2S64 candidates[] = {
+                v2s64_add(point, v2s64( 1,  0)),
+                v2s64_add(point, v2s64( 0,  1)),
+                v2s64_add(point, v2s64(-1,  0)),
+                v2s64_add(point, v2s64( 0, -1)),
+            };
 
-    if (wanted == '9' && map[index] == 'X') {
-        return v2u64(0, 1);
-    } else if (map[index] != wanted) {
-        return v2u64(0, 0);
-    } else if (map[index] == '9') {
-        map[index] = 'X';
-        return v2u64(1, 1);
-    }
-
-    V2S64 candidates[] = {
-        v2s64_add(point, v2s64( 1,  0)),
-        v2s64_add(point, v2s64( 0,  1)),
-        v2s64_add(point, v2s64(-1,  0)),
-        v2s64_add(point, v2s64( 0, -1)),
-    };
-
-    V2U64 result = v2u64(0, 0);;
-    U8 new_wanted = wanted + 1;
-    for (U64 i = 0; i < array_count(candidates); ++i) {
-        if (r2s64_contains(bounds, candidates[i])) {
-            result = v2u64_add(result, day10_find_9(map, bounds, candidates[i], new_wanted));
+            for (U64 i = 0; i < array_count(candidates); ++i) {
+                if (r2s64_contains(bounds, candidates[i])) {
+                    result = v2u64_add(result, day10_find_9(visited, map, bounds, candidates[i], wanted + 1));
+                }
+            }
         }
     }
+
     return result;
 }
 
@@ -50,31 +47,19 @@ internal Void day10_solve(Void) {
     S64 height = (S64) lines.node_count;
     Str8 map = str8_join(arena, &lines);
 
-    V2S64List heads = { 0 };
+    V2U64 accumulator = v2u64(0, 0);
     for (U64 i = 0; i < map.size; ++i) {
         if (map.data[i] == '0') {
-            V2S64Node *node = arena_push_struct_zero(arena, V2S64Node);
-            node->value.x = (S64) i % width;
-            node->value.y = (S64) i / width;
-            sll_queue_push(heads.first, heads.last, node);
+            Arena_Temporary scratch = arena_get_scratch(0, 0);
+            B8 *visited = arena_push_array_zero(arena, B8, map.size);
+            V2U64 stats = day10_find_9(visited, map.data, r2s64(0, 0, width, height), v2s64((S64) i % width, (S64) i / width), '0');
+            accumulator = v2u64_add(accumulator, stats);
+            arena_end_temporary(scratch);
         }
     }
 
-    U64 score = 0;
-    U64 rating = 0;
-    for (V2S64Node *head = heads.first; head; head = head->next) {
-        V2U64 stats = day10_find_9(map.data, r2s64(0, 0, width, height), head->value, '0');
-        score += stats.x;
-        rating += stats.y;
-        for (U64 i = 0; i < map.size; ++i) {
-            if (map.data[i] == 'X') {
-                map.data[i] = '9';
-            }
-        }
-    }
-
-    os_console_print(str8_format(arena, "Part 1: %lu\n", score));
-    os_console_print(str8_format(arena, "Part 2: %lu\n", rating));
+    os_console_print(str8_format(arena, "Part 1: %lu\n", accumulator.x));
+    os_console_print(str8_format(arena, "Part 2: %lu\n", accumulator.y));
 
     arena_destroy(arena);
 }
